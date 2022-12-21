b7376705ed6d8f25f66060fe078b4602fb82ee8b
Sonwon112 <tlatmsdkqk@gmail.com>
2022-12-21 15:02:31 +0900

    라이프 사이클 표현

--- src/javaFXStart/AppMian.java
+++ src/javaFXStart/AppMian.java
@@ -5,12 +5,28 @@ import javafx.stage.Stage;
 
 public class AppMian extends Application{
 	
+	public AppMian() {
+		System.out.println(Thread.currentThread().getName()+": AppMain() 호출");
+	}
+	
+	@Override
+	public void init() throws Exception{
+		System.out.println(Thread.currentThread().getName()+": init() 호출");
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
+		System.out.println(Thread.currentThread().getName()+": start() 호출");
 		primaryStage.show();
 	}
 	
+	@Override
+	public void stop() {
+		System.out.println(Thread.currentThread().getName()+": stop() 호출");
+	}
+	
 	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName()+": main() 호출");
 		launch(args);
 	}
 }
